09775b3741b4d7773fdaaa7e878da5cd6ea58e08
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-09 18:17:35 +0200

    Aggiunta AppTitle.vue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>{{ message }}</h1>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        message: "La mia prima app con Vite!"
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  h1 {
+    font-size: 18px;
+    color: red;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  </style>
